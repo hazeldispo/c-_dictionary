2ebdf978cdbc5249c345145875b8754e58cdf79c
hazeldispo <hg.dispo94@gmail.com>
2025-03-30 20:16:58 +0800

    Add files via upload

---
+++ Dictionary c++.cpp
@@ -0,0 +1,79 @@
+#include <iostream>
+#include <string>
+
+using namespace std;
+
+int main() {
+    string engg_term;
+    
+    cout << "Enter an engineering term:";
+    cin >> engg_term;
+    
+    if (engg_term == "ACCELERATION"){
+        cout << "ACCELERATION: The consequences of force and mass" << endl;
+    } else if (engg_term == "AGGREGATE"){
+        cout << "AGGREGATE: Crushed stone, sand, or gravel used in construction materials" << endl;
+    } else if (engg_term == "ANODE"){
+        cout << "ANODE: This is where oxidation occurs" << endl;
+    } else if (engg_term == "ASPHALT"){
+        cout << "ASPHALT: A petroleum-based substance for waterproofing and fixing roads" << endl;
+    } else if (engg_term == "BLUEPRINT"){
+        cout << "BLUEPRINT: A complex technical drawing used in engineering projects" << endl;
+    } else if (engg_term == "CANDELA"){
+        cout << "CANDELA: The SI unit of luminous intensity" << endl;
+    } else if (engg_term == "CATHODE"){
+        cout << "CATHODE: This is where the reduction occurs" << endl;
+    } else if (engg_term == "CEMENT"){
+        cout << "CEMENT: Materials for binding that are used in mortar and concrete" << endl;
+    } else if (engg_term == "CONCRETE"){
+        cout << "CONCRETE: A mixture of cement, water, sand, and gravel used in construction" << endl;
+    } else if (engg_term == "DENSITY"){
+        cout << "DENSITY: The mass per unit volume of a substance" << endl;
+    } else if (engg_term == "DIFFERENTIATION"){
+        cout << "DIFFERENTIATION: A concept in calculus used to find the rate of change of a function" << endl;
+    } else if (engg_term == "FOUNDATION"){
+        cout << "FOUNDATION: A structures base that transmits its weight to the surface" << endl;
+    } else if (engg_term == "GENERATOR"){
+        cout << "GENERATOR: Equipment that produces electrical energy from mechanical energy" << endl;
+    } else if (engg_term == "HERTZ"){
+        cout << "HERTZ: The SI unit of frequency, which measures cycles per second" << endl;
+    } else if (engg_term == "HORSEPOWER"){
+        cout << "HORSEPOWER: The English unit of power, commonly used for engines" << endl;
+    } else if (engg_term == "INTEGRATION"){
+        cout << "INTEGRATION: A mathematical process that is the reverse process of differentiation" << endl;
+    } else if (engg_term == "JOULE"){
+        cout << "JOULE: The SI unit of energy" << endl;
+    } else if (engg_term == "KELVIN"){
+        cout << "KELVIN: The SI unit of temperature measurement" << endl;
+    } else if (engg_term == "MASONRY"){
+        cout << "MASONRY: The construction of structures using bricks, stones, or concrete blocks" << endl;
+    } else if (engg_term == "MOLE"){
+        cout << "MOLE: The SI unit of amount of substance" << endl;
+    } else if (engg_term == "MORTAR"){
+        cout << "MORTAR: A mixture of cement, water, and sand used in bonding masonry units" << endl;
+    } else if (engg_term == "NEOPRENE"){
+        cout << "NEOPRENE: A synthetic rubber that is used for waterproofing" << endl;
+    } else if (engg_term == "NEWTON"){
+        cout << "NEWTON: The SI unit of force in physics" << endl;
+    } else if (engg_term == "OHM"){
+        cout << "OHM: The unit of electical resistance" << endl;
+    } else if (engg_term == "PASCAL"){
+        cout << "PASCAL: The SI unit of pressure measurement" << endl;
+    } else if (engg_term == "QUARRY"){
+         cout << "QUARRY: A site whre stone, gravel, or minerals are extracted" << endl;
+    } else if (engg_term == "SCALAR"){
+         cout << "SCALAR: A quantity that has magnitude but no direction" << endl;
+    } else if (engg_term == "TENSILE STRENGTH"){
+        cout << "TENSILE STRENGTH: The resistance of a material to breaking when under pressure" << endl;
+   } else if (engg_term == "VECTOR"){  
+       cout << "VECTOR: A quantity that has both magnitude and direction" << endl;
+    } else if (engg_term == "WATT"){ 
+        cout << "WATT: The SI unit of power" << endl;
+    } else {   
+        cout << "Invalid input." << endl;
+    }
+    
+    return 0;
+    
+}
+                                                                         
